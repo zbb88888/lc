520e8c51eafb6f9fe705a4ffb89dd88ee4efb216
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-06-26 11:30:51 +0800

    pass 142 / 142
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

--- 0625-longestPalindrome.go
+++ 0625-longestPalindrome.go
@@ -1,75 +1,51 @@
 package main
+import (
+  "fmt"
+)
+
 
 func longestPalindrome(s string) string {
-
-// 这个是一个栈的操作思路
-// 需要一个切片，存储最长的字符串
-// 字符串不在切片中，存入
-// 字符串在切片中，判断是否回文
-// 清空：
-// 1. 从栈底部开始清空不再符合回文的部分
-// 2. 回文结束，清空整个回文的部分，判断是否是最长的回文片段并记录
-// 继续迭代
-
-
-long := []string // 缓存最长回文
-
-for v := range s {
-  if !contain(long, v) {
-    long = append(long, v)
-    continue
+  max := 0
+  result := ""
+  // 初始化滑动窗口
+  lens := len(s)
+  for window:=1; window <= lens; window ++ {
+    for i:=0; i <= lens-window; i ++ {
+      // 缓存窗口大小，判断
+      long := s[i:i+window]
+      yes := check(long)
+      fmt.Println("check: matched ?", yes, long)
+      if yes && len(long) > max {
+          result = long 
+          println("find longest: ", result)
+      }
+    }
   }
+  return result
+}
 
-  
-  long = append(long, v)
+func check(s string)  bool {
   // 判断当前是否依旧符合回文
-
-}
-    
-}
-
-func check(list []string) (list []string, bool) {
-  maxIndex := len(list)-1
+  maxIndex := len(s)-1
   mid := maxIndex/2
-  goby := 0
-
   for i:=0; i <= mid; i++ {
-    if list[i] == list[maxIndex-1] {
-      // 符合回文，继续
-      // 如果到最后 mid, 则就是一个完整的回文字符串
-      println("match at : ", i, maxIndex-1, "both are", list[i])
-      goby = i
+    if s[i] == s[maxIndex-i] {
       continue
-    }
-    println("not match at: ", i, maxIndex-1, list[i], "!=", list[maxIndex - 1])
-    // 不再符合，则清理
-    break
-  }
-
-  if goby == mid {
-    // 始终符合回文
-     return list, true
-  }
-
-  // 不再符合，则从底部开始清理
-  println("before cut from: ", goby)
-  newList := list[goby:]
-  println("after cut from: ", goby)
-}
-
-func contain(list []string, s string) bool {
-  for v := range list{
-    if v == s {
-      return true
+    }else { 
+      return false
     }
   }
-  return false
+   return true
 }
-
 
 func main(){
 
-s := "babad"
+// 这是一个滑动窗口题
+// 窗口大小 从 1 到 len(s)
+// 每次取窗口大小的list，然后判断是否回文，然后步进一，继续
+// 穷举的核心就是： 缓存 判断 缓存 继续
+s := "aacabdkacaa"
+println("input: ", s)
 
 result := longestPalindrome(s)
 
