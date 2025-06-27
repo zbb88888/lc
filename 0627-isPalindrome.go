87ac67900d400f6d0fb68599b8af8f5221aa505d
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-06-27 14:50:03 +0800

    init
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

---
+++ 0627-isPalindrome.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+)
+
+
+func isPalindrome(x int) bool {
+// 不将整数转为字符串来解决
+// 使用进制的方式把这个数反转过来相等则回文
+    
+}
+
+
+
+func main(){
+
+input: = 123
+
+isPalindrome(input)
+
+}
