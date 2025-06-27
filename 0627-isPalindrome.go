b72cb0a775594fd73fde1fcc3dbf5a739bd67d38
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-06-28 00:33:00 +0800

    update
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

--- 0627-isPalindrome.go
+++ 0627-isPalindrome.go
@@ -8,6 +8,23 @@ import (
 func isPalindrome(x int) bool {
 // 不将整数转为字符串来解决
 // 使用进制的方式把这个数反转过来相等则回文
+
+
+  if x < 0 {
+    return false
+  }
+
+  if x < 10 {
+     return true
+  }
+
+
+  if x == 10 {
+    return false
+  }
+
+
+
     
 }
 
