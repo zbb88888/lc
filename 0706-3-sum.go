8424d324d9c56a19bccc5a2a596ed75371c092ef
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-07-08 14:42:39 +0800

    not fast
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

--- 0706-3-sum.go
+++ 0706-3-sum.go
@@ -15,9 +15,9 @@ func main() {
 }
 
 // 简化思路
+// 如果要求排序的话，先排序，然后再找两个数，等 nums[i] > 0 就可以结束了
 // 先减去一个值，然后找两个数是否能够凑够剩下的和
 // 如果要求不排序的话，这里就已经是最佳方案
-// 如果要求排序的话，先排序，然后再找两个数，等 nums[i] > 0 就可以结束了
 
 func threeSum(nums []int) [][]int {
 	// 用于存储 i 的 v：为 j + k
@@ -31,11 +31,55 @@ func threeSum(nums []int) [][]int {
 	var key string
 	var l2Got []int
 	l := len(nums)
+	slices.Sort(nums) // 先排序
+	// fmt.Println("sorted nums:", nums)
+	var newNums []int
+	match0 := 0
+	last2Same := false
+	newNums = append(newNums, nums[0])
+	last := 0
+	for i = 1; i < l; i++ {
+		// 预处理
+		// 快速跳过连续重复的值
+		// 当重复的值超过两个时，才可以跳过
+		// 0 最多连续保留四个
+		if nums[i] == 0 {
+			if match0 >= 4 {
+				continue
+			}
+			match0++
+			newNums = append(newNums, nums[i])
+			last++
+			continue
+		}
+		// 其他的值最多连续保留两个
+		if newNums[last] != nums[i] {
+			newNums = append(newNums, nums[i])
+			last++
+			last2Same = false
+		} else {
+			if last2Same {
+				continue
+			} else {
+				newNums = append(newNums, nums[i])
+				last++
+				last2Same = true
+				continue
+			}
+		}
+	}
+
+	// fmt.Println("newNums:", newNums)
+	l = len(newNums)
 	for i = 0; i < l; i++ {
-		sum = 0 - nums[i]
+		if newNums[i] > 0 {
+			// fmt.Println("v > 0")
+			return ret
+		}
+		sum = 0 - newNums[i]
 		for j = i + 1; j < l; j++ {
 			// 这里优先排序, 更容易匹配
-			l2Got = []int{nums[i], nums[j]}
+			l2Got = []int{newNums[i], newNums[j]}
 			slices.Sort(l2Got)
 			l2Builder.Reset()
 			l2Builder.WriteString(strconv.Itoa(l2Got[0]))
@@ -45,10 +89,10 @@ func threeSum(nums []int) [][]int {
 				// fmt.Println("skip:", key)
 				continue
 			}
-			l3v := sum - nums[j]
+			l3v := sum - newNums[j]
 			for k = j + 1; k < l; k++ {
-				if l3v == nums[k] {
-					tmp := append(l2Got, nums[k])
+				if l3v == newNums[k] {
+					tmp := append(l2Got, newNums[k])
 					slices.Sort(tmp)
 					// 只需要缓存两个值就足够
 					l3Builder.Reset()
@@ -62,7 +106,7 @@ func threeSum(nums []int) [][]int {
 					}
 					ijkV[key] = true
 					// fmt.Println("cache:", key)
-					ret = append(ret, []int{nums[i], nums[j], nums[k]})
+					ret = append(ret, []int{newNums[i], newNums[j], newNums[k]})
 				}
 			}
 		}
